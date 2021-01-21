bdd391662dbe9b793dc58587d32e02d3160e3369
nro337 <nro337@gmail.com>
2021-01-21 11:30:30 -0500

    Handled User Auth between Google and platform Sign-up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,9 +13,35 @@ const config = {
 
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get()
+
+  if(!snapShot.exists) {
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('Error creating user', error.message);
+    }
+  }
+
+  //may want this for other purposes
+  return userRef;
+}
+
 export const auth = firebase.auth();
 // eslint-disable-next-line
-// export const firestore = firestore.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
